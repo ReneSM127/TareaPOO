b9eb54d43246514602ba1a76374a02de5d3902f0
ReneSM127 <101184543+ReneSM127@users.noreply.github.com>
2022-04-02 18:33:34 -0500

    Add files via upload

---
+++ TCPPrueba.py
@@ -0,0 +1,18 @@
+from http import client
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the client
+client.connect((target_host,target_port))
+
+# Send some data
+client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+# Recieve some data
+response = client.recv(4096)
+
+# Sandoval Moron Rene
---
+++ UDPprueba.py
@@ -0,0 +1,16 @@
+from http import client
+import socket
+target_host = "127.0.0.1"
+target_port = 80
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Send some data
+client.sendto("AAABBBCCC",(target_host,target_port))
+
+# Receive some data
+data, addr = client.recvfrom(4096)
+
+print(data)
+
+# Sandoval Moron Rene
